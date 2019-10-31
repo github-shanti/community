e9fc65e286af35b1cb6ff25a30540aff7125f153
kun <shanticc@163.com>
2019-11-01 03:35:47 +0800

    add login token

--- src/main/java/life/majiang/community/controller/AuthorizeController.java
+++ src/main/java/life/majiang/community/controller/AuthorizeController.java
@@ -2,6 +2,8 @@ package life.majiang.community.controller;
 
 import life.majiang.community.dto.AccessTokenDTO;
 import life.majiang.community.dto.GithubUser;
+import life.majiang.community.mapper.UserMapper;
+import life.majiang.community.model.User;
 import life.majiang.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,12 +11,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider ;
+    @Autowired
+    private UserMapper userMapper;
     @Value("${github.client.id}")
     private String clientId;
     @Value("${github.client.secret}")
@@ -25,7 +32,7 @@ public class AuthorizeController {
     @GetMapping("/callback")
     public String callback(
             @RequestParam(name = "code") String code,
-            @RequestParam(name = "state") String state, HttpServletRequest request) {
+            @RequestParam(name = "state") String state, HttpServletRequest request, HttpServletResponse response) {
 
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO(
                 clientId,
@@ -37,11 +44,22 @@ public class AuthorizeController {
 
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         System.out.println("accessToken = " + accessToken);
-        GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println("user = " + user);
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        System.out.println("githubUser = " + githubUser);
 
-        if (user != null) {
-            request.getSession().setAttribute("user",user);
+        if (githubUser != null) {
+
+            User user = new User();
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setMgtModified(System.currentTimeMillis());
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
+            userMapper.inster(user);
+            response.addCookie(new Cookie("token",token));
+
+//            request.getSession().setAttribute("githubUser",githubUser);
             return "redirect:/";
         } else {
             return "redirect:/";
--- src/main/java/life/majiang/community/controller/IndexController.java
+++ src/main/java/life/majiang/community/controller/IndexController.java
@@ -1,12 +1,21 @@
 package life.majiang.community.controller;
 
+import life.majiang.community.mapper.UserMapper;
+import life.majiang.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class IndexController {
+
+    @Autowired
+    private UserMapper userMapper;
 
     @GetMapping("/hello")
     public String hello(@RequestParam(name = "age", required = false) String name, Model model) {
@@ -15,7 +24,18 @@ public class IndexController {
     }
 
     @GetMapping("/")
-    public String index() {
+    public String index(HttpServletRequest request) {
+
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("token")) {
+                User user = userMapper.findByToken(cookie.getValue());
+                if (user!=null) {
+                    request.getSession().setAttribute("user",user);
+                }
+                break;
+            }
+        }
         System.out.println("------哈哈哈哈哈哈-------------");
         return "index";
     }
